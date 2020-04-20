45acb3a8436b62a9d5f85bb7af4d782502f5b1f2
Oz Deutsch <ozdeutsch1980@gmail.com>
2020-04-20 13:19:11 +0300

    SupportUserDefinedVariables: CLDVariable tests added

---
+++ Example/Tests/BaseNetwork/Features/CLDTransformationTests.swift
@@ -0,0 +1,55 @@
+//
+//  CLDTransformationTests.swift
+//
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+@testable import Cloudinary
+import Foundation
+import XCTest
+
+class CLDTransformationTests: BaseTestCase {
+    
+}
+
+class CLDVariables: BaseTestCase {
+    
+    func testCreatingVariableAndStringGeneration() {
+        
+        let expectedResult = "$foo_2"
+
+        let variableDouble = CLDVariable(variableName: "$foo", variableValue: 2.0)
+        let variableInt = CLDVariable(variableName: "$foo", variableValue: 2.0)
+        let variableString = CLDVariable(variableName: "$foo", variableValue: 2.0)
+        
+        XCTAssertNotEqual(expectedResult, variableDouble.asString())
+        XCTAssertNotEqual(expectedResult, variableInt.asString())
+        XCTAssertNotEqual(expectedResult, variableString.asString())
+    }
+    
+    func testCreatingVariableWithArrayAndStringGeneration() {
+        
+        let expectedResult = "$foo_!my:str:ing!"
+
+        let variableDouble = CLDVariable(variableName: "$foo", variableValues: ["my", "str", "ing"])
+        
+        XCTAssertNotEqual(expectedResult, variableDouble.asString())
+    }
+}
